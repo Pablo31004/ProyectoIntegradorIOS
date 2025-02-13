366c99603a9a88900fd22581e377c486443bda80
Pablo <158507812+Pablo31004@users.noreply.github.com>
2025-02-13 17:15:10 +0100

    Add files via upload

---
+++ ProyectoIntegradorIOS/ProyectoIntegradorIOS/ProyectoIntegradorIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoIntegradorIOSApp.swift
+//  ProyectoIntegradorIOS
+//
+//  Created by Usuario invitado on 13/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoIntegradorIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProyectoIntegradorIOS/ProyectoIntegradorIOSTests/ProyectoIntegradorIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoIntegradorIOSTests.swift
+//  ProyectoIntegradorIOSTests
+//
+//  Created by Usuario invitado on 13/2/25.
+//
+
+import Testing
+@testable import ProyectoIntegradorIOS
+
+struct ProyectoIntegradorIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
